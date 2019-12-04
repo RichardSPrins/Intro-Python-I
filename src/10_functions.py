db67c47ed9c19713cef991d67a5d23ed33a0a7d1
Richard Scott Prins Jr <richardsprins@gmail.com>
2019-12-04 16:38:08 -0500

    adding new version of projects for cs24

--- src/10_functions.py
+++ src/10_functions.py
@@ -1,6 +1,12 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
+def even(num):
+    if num % 2 == 0:
+      return True
+    else:
+      return False
+
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
@@ -9,4 +15,11 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+def print_even(num):
+    if num % 2 == 0:
+      print('Even!')
+    else:
+      print('Odd')
+    
 
+print_even(num)
